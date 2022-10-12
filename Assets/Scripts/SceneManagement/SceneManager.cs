924b8544a29d9c25cfb9494ae811c2e6ca88fa6a
HFurkanBirel <hidayetfurkanbirel@gmail.com>
2022-10-12 15:52:42 +0300

    Scene Manager script added.
    SceneManager will turn a next scene selector. I'll add a dropdown menu for selection.

---
+++ Assets/Scripts/SceneManagement/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿ using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static string[] scenes;
+
+    void Awake()
+    {
+        var sceneNumber = SceneManager.sceneCountInBuildSettings;
+        scenes = new string[sceneNumber];
+        for (int i = 0; i < sceneNumber; i++)
+        {
+            scenes[i] = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+        }
+    }
+}
